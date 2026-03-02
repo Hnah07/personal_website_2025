97f5d60be18f4965ca9774e904b012fc51a1eb9c
Hannah Casier <hannah.casier@me.com>
2026-03-02 14:06:34 +0100

    start read in crud

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export default function AdminDashboardPage() {
   return (
     <main className="flex flex-col items-center">
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="pt-20">Admin Dashboard</h1>
         <p>Welcome to the Admin Dashboard page!</p>
+        <Link
+          href="/admin/trips"
+          className="px-3 bg-gradient-to-r from-brilliant-rose to-verdigris text-white rounded-lg hover:opacity-90 transition-opacity"
+        >
+          Go to Trips Admin Page
+        </Link>
       </div>
     </main>
   );
--- app/admin/trips/page.tsx
+++ app/admin/trips/page.tsx
@@ -1,9 +1,27 @@
-export default function CRUDPage() {
+import { createClient } from "@/utils/supabase/server";
+
+export default async function CRUDPage() {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("trips")
+    .select("*")
+    .order("year", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return <p>Something went wrong...</p>;
+  }
   return (
     <main className="flex flex-col items-center">
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="pt-20">Trips Admin Page</h1>
-        <p>Welcome to the Trips Admin CRUD page!</p>
+        <ul>
+          {data.map((trip) => (
+            <li key={trip.id}>
+              {trip.title} - {trip.year}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
