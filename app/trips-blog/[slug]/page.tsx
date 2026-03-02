77209abdf4b0f84d21a3abc148f9e781432cf170
Hannah Casier <hannah.casier@me.com>
2026-03-02 16:15:32 +0100

    added a default function so build can run

---
+++ app/trips-blog/[slug]/page.tsx
@@ -0,0 +1,10 @@
+export default function TripView() {
+  return (
+    <main className="flex flex-col items-center">
+      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="pt-20">Trip View</h1>
+        <p>This is going to be a trip!</p>
+      </div>
+    </main>
+  );
+}
