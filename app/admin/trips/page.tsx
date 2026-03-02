521c90b2290bf6438394c2ed79ea6e33876c7b59
Hannah Casier <hannah.casier@me.com>
2026-03-02 18:05:14 +0100

    added the crud table

--- app/admin/trips/page.tsx
+++ app/admin/trips/page.tsx
@@ -1,4 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
+import TripsCrudTable from "@/components/TripsCrudTable";
 
 export default async function CRUDPage() {
   const supabase = await createClient();
@@ -12,16 +13,10 @@ export default async function CRUDPage() {
     return <p>Something went wrong...</p>;
   }
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col">
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="pt-20">Trips Admin Page</h1>
-        <ul>
-          {data.map((trip) => (
-            <li key={trip.id}>
-              {trip.title} - {trip.year}
-            </li>
-          ))}
-        </ul>
+        <h1 className="pt-20 mb-8">Trips Admin Page</h1>
+        <TripsCrudTable data={data} />
       </div>
     </main>
   );
