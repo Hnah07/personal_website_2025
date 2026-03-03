ee8bd2f72d474f33b927fde7267432ef2ba53ab5
Hannah Casier <hannah.casier@me.com>
2026-03-03 17:51:47 +0100

    added link to go back to dashboard + added the header

--- app/admin/trips/page.tsx
+++ app/admin/trips/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import TripsCrudTable from "@/components/TripsCrudTable";
+import Header from "@/components/layout/Header";
+import Link from "next/link";
 
 export default async function CRUDPage() {
   const supabase = await createClient();
@@ -14,8 +16,16 @@ export default async function CRUDPage() {
   }
   return (
     <main className="flex flex-col">
+      <Header />
+      {/* add a back link to the admin dashboard */}
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="pt-20 mb-8">Trips Admin Page</h1>
+        <Link
+          href="/admin"
+          className="ml-4 my-4 flex items-center text-sm text-gray-500 hover:text-gray-700"
+        >
+          ← Back to Admin Dashboard
+        </Link>
         <TripsCrudTable data={data} />
       </div>
     </main>
