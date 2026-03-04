c3521da97b9ba7cb8b1279c717e5eab66e6d5f24
Hannah Casier <hannah.casier@me.com>
2026-03-04 19:14:24 +0100

    added a date range picker

--- components/admin/AddTripForm.tsx
+++ components/admin/AddTripForm.tsx
@@ -2,11 +2,12 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import DatePickerRange from "../DatePickerRange";
 
 export default function AddTripForm() {
   return (
     <form className="w-full max-w-lg">
-      <div className="flex flex-wrap -mx-3 mb-6 w-full">
+      <div className="flex flex-wrap -mx-3 mb-6 w-full gap-4">
         <div className="w-full px-3 mb-6 md:mb-0">
           <Label htmlFor="Title">Title</Label>
           <Input
@@ -15,9 +16,7 @@ export default function AddTripForm() {
             placeholder="Write here the title of your trip"
           />
         </div>
-        <div className="w-full px-3">
-          <Label htmlFor="Location">Location</Label>
-        </div>
+        <DatePickerRange />
       </div>
     </form>
   );
