0619f30f43ad8e535748aeac64c11e6e5320a25f
Hannah Casier <hannah.casier@me.com>
2026-03-06 12:05:01 +0100

    added a react hook form, and adjusted the form accordingly

--- components/admin/AddTripForm.tsx
+++ components/admin/AddTripForm.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 import DatePickerRange from "../DatePickerRange";
 import { useState } from "react";
 import { type DateRange } from "react-day-picker";
@@ -18,7 +19,7 @@ import countries from "country-list";
 import Dropzone from "./Dropzone";
 // import TripContentBlocks from "./TripContentBlocks";
 import dynamic from "next/dynamic";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { TripFormData } from "@/types";
 
 const TripContentBlocks = dynamic(() => import("./TripContentBlocks"), {
@@ -28,61 +29,80 @@ const TripContentBlocks = dynamic(() => import("./TripContentBlocks"), {
 export default function AddTripForm() {
   const [date, setDate] = useState<DateRange | undefined>();
   const countryList = countries.getNames();
-  const [excerpt, setExcerpt] = useState("");
-  const [published, setPublished] = useState(false);
+  // const [excerpt, setExcerpt] = useState("");
+  // const [published, setPublished] = useState(false);
   const [heroImage, setHeroImage] = useState<File | null>(null);
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<TripFormData>();
+
+  const excerptValue = watch("excerpt");
+
   return (
-    <form
-      className="w-full"
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
+    <form className="w-full">
       <div className="flex flex-col -mx-3 mb-6 w-full gap-4">
         <div className="w-full px-3 mb-6 md:mb-0">
-          <Label htmlFor="Title">Title</Label>
+          <Label htmlFor="title">Title</Label>
           <Input
-            id="Title"
+            id="title"
             type="text"
             placeholder="Write here the title of your trip"
+            {...register("title")}
           />
         </div>
         <DatePickerRange date={date} onDateChange={setDate} />
         <div className="w-full px-3 mb-6 md:mb-0">
-          <Label htmlFor="Country">Country</Label>
-          <Select>
-            <SelectTrigger className="w-full" id="Country">
-              <SelectValue placeholder="Select a country" />
-            </SelectTrigger>
-            <SelectContent>
-              {countryList.map((country) => (
-                <SelectItem key={country} value={country}>
-                  {country}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+          <Label htmlFor="country">Country</Label>
+          <Controller
+            control={control}
+            name="country"
+            render={({ field }) => (
+              <Select onValueChange={field.onChange} value={field.value}>
+                <SelectTrigger className="w-full" id="country">
+                  <SelectValue placeholder="Select a country" />
+                </SelectTrigger>
+                <SelectContent>
+                  {countryList.map((country) => (
+                    <SelectItem key={country} value={country}>
+                      {country}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          />
         </div>
         <div className="px-3 mb-6 md:mb-0">
           <Label htmlFor="location-type">Location Type</Label>
-          <Select>
-            <SelectTrigger className="w-full" id="location-type">
-              <SelectValue placeholder="Select a location type" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="city">City</SelectItem>
-              <SelectItem value="region">Region</SelectItem>
-              <SelectItem value="country">Country</SelectItem>
-            </SelectContent>
-          </Select>
+          <Controller
+            control={control}
+            name="location_type"
+            render={({ field }) => (
+              <Select onValueChange={field.onChange} value={field.value}>
+                <SelectTrigger className="w-full" id="location_type">
+                  <SelectValue placeholder="Select a location type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="city">City</SelectItem>
+                  <SelectItem value="region">Region</SelectItem>
+                  <SelectItem value="country">Country</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
         </div>
         <div className="px-3 mb-6 md:mb-0">
-          <Label htmlFor="location-name">Name of the location</Label>
+          <Label htmlFor="location_name">Name of the location</Label>
           <Input
-            id="location-name"
+            id="location_name"
             type="text"
             placeholder="Enter the name of the location"
+            {...register("location_name")}
           />
         </div>
         <div className="px-3 mb-6 md:mb-0">
@@ -94,11 +114,10 @@ export default function AddTripForm() {
               id="excerpt"
               placeholder="Write a small excerpt of the trip to show in the trip list"
               maxLength={160}
-              value={excerpt}
-              onChange={(e) => setExcerpt(e.target.value)}
+              {...register("excerpt")}
             />
             <p className="text-sm text-muted-foreground absolute bottom-1 right-1">
-              {excerpt.length}/160
+              {excerptValue?.length ?? 0}/160
             </p>
           </div>
         </div>
@@ -117,16 +136,27 @@ export default function AddTripForm() {
         <div className="px-3 mb-6 md:mb-0">
           <Label htmlFor="is-published">Publish this trip?</Label>
           <div className="flex items-center space-x-2">
-            <Checkbox
-              id="is-published"
-              checked={published}
-              onCheckedChange={(checked) => setPublished(checked === true)}
+            <Controller
+              control={control}
+              name="published"
+              render={({ field }) => (
+                <Checkbox
+                  id="is-published"
+                  // checked={published}
+                  // onCheckedChange={(checked) => setPublished(checked === true)}
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              )}
             />
             <p className="text-sm">Yes, publish this trip</p>
           </div>
         </div>
         <TripContentBlocks />
       </div>
+      <Button type="submit" className="ml-3">
+        Save Trip
+      </Button>
     </form>
   );
 }
