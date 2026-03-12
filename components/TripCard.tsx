2c6f155208bcd19075a3d3362b863af2d0e3a6f3
Hannah Casier <hannah.casier@me.com>
2026-03-12 21:37:38 +0100

    added badges in hero image

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -7,6 +7,8 @@ import {
   CardContent,
 } from "@/components/ui/card";
 import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import formatDate from "@/utils/formatDate";
 
 export default function TripCard({
   heroImage,
@@ -27,12 +29,30 @@ export default function TripCard({
 }) {
   return (
     <Card className="mb-8 w-1/3">
-      <Image src={heroImage} alt={title} width={150} height={100}></Image>
+      <div className="relative">
+        <Image src={heroImage} alt={title} width={150} height={100}></Image>
+        <div className="absolute top-2 right-2 flex flex-col gap-2">
+          <div className="flex flex-wrap gap-2">
+            {location.map((l) => (
+              <Badge key={l} variant="secondary">
+                {l}
+              </Badge>
+            ))}
+          </div>
+          <div className="flex flex-wrap gap-2 flex-row-reverse">
+            {country.map((c) => (
+              <Badge key={c} variant="secondary">
+                {c}
+              </Badge>
+            ))}
+          </div>
+        </div>
+      </div>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
         <CardDescription>
-          {location}, {country}
+          {formatDate(startDate)} - {formatDate(endDate)}
         </CardDescription>
+        <CardTitle>{title}</CardTitle>
       </CardHeader>
       <CardContent>
         <p>{excerpt}</p>
