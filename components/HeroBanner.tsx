8cf6e84ef9c954d3c764fae08ec6bd04bc0fa19e
Hannah Casier <hannah.casier@me.com>
2026-03-13 16:31:42 +0100

    styling

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-// import formatDate from "@/utils/formatDate";
+import formatDate from "@/utils/formatDate";
 import Link from "next/link";
 import Image from "next/image";
 import { Trip } from "@/types/index";
 import { useEffect, useState } from "react";
+import { CardDescription } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 export default function HeroBanner({ trips }: { trips: Trip[] }) {
   const tripsWithHeroBanner = trips.filter((t) => t.hero_image);
@@ -30,6 +32,26 @@ export default function HeroBanner({ trips }: { trips: Trip[] }) {
           fill
           className="object-cover"
         ></Image>
+        <div className="absolute bottom-4 left-4 lg:max-w-[50%] max-w-[70%]">
+          <div className="flex flex-wrap gap-2 mb-4">
+            {currentTrip.country.map((c) => (
+              <Badge
+                key={c}
+                variant="secondary"
+                className="bg-brilliant-rose-light text-eerie-black dark:hover:bg-brilliant-rose-light/80"
+              >
+                {c}
+              </Badge>
+            ))}
+          </div>
+          <h3 className="leading-none">{currentTrip.title}</h3>
+          <p className="text-sm text-parchment">{currentTrip.excerpt}</p>
+          <CardDescription>
+            {currentTrip.start_date === currentTrip.end_date
+              ? formatDate(currentTrip.start_date.toString(), true)
+              : `${formatDate(currentTrip.start_date.toString(), true)} - ${formatDate(currentTrip.end_date!.toString(), true)}`}
+          </CardDescription>
+        </div>
       </Link>
     </div>
   );
