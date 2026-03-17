9bc92a3e2a0ddd8fe18c9c06f1aaecd42213b204
Hannah Casier <hannah.casier@me.com>
2026-03-17 17:50:11 +0100

    adjusted hero banner for single trips

--- app/trips-blog/[slug]/page.tsx
+++ app/trips-blog/[slug]/page.tsx
@@ -1,10 +1,35 @@
-export default function TripView() {
+import { createClient } from "@/utils/supabase/server";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import HeroBanner from "@/components/HeroBanner";
+
+export default async function TripView({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const supabase = await createClient();
+  const { data: trip, error } = await supabase
+    .from("trips")
+    .select("*")
+    .eq("slug", slug)
+    .single();
+
+  if (error) {
+    console.error(error);
+    return <p>Something went wrong...</p>;
+  }
+
   return (
     <main className="flex flex-col items-center">
+      <Header />
+      <HeroBanner trip={trip} />
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="pt-20">Trip View</h1>
         <p>This is going to be a trip!</p>
       </div>
+      <Footer />
     </main>
   );
 }
