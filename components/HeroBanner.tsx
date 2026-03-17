f8cca44d54a47ca4dd6dd5b4a2e762601b89210e
Hannah Casier <hannah.casier@me.com>
2026-03-17 18:54:53 +0100

    adjusted overlay

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -45,7 +45,9 @@ export default function HeroBanner({
         fill
         className="object-cover"
       ></Image>
-      <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+
+      <div className="absolute inset-0 bg-gradient-to-t from-black/70 from-0% to-transparent to-30%"></div>
+
       <div className="absolute bottom-4 left-4 lg:max-w-[50%] max-w-[70%]">
         <div className="flex flex-wrap gap-2 mb-4 ">
           {currentTrip.country.map((c) => (
