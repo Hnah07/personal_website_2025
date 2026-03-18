78fc1e09003daffd869118e64e31c1dd1080b416
Hannah Casier <hannah.casier@me.com>
2026-03-18 20:02:53 +0100

    added disable comment

--- types/index.ts
+++ types/index.ts
@@ -38,7 +38,8 @@ export type { TripFormData };
 type EditorBlock = {
   type: string;
   id: string;
-  data: Record<string, unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: Record<string, any>;
 };
 
 export type { EditorBlock };
